e49298b08f8264965b37330118f6053708add828
masha14013 <masha14013@rambler.ru>
2022-10-15 23:02:20 +0300

    file changed

--- src/index.ts
+++ src/index.ts
@@ -2,39 +2,70 @@ import express, {Request, Response} from 'express'
 import bodyParser from 'body-parser'
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
 const videos = [
     {
         id: 1,
         title: "hello",
         author: "Joe"
+    },
+    {
+        id: 2,
+        title: "world",
+        author: "Ann"
     }
 ]
 
 const parserMiddleware = bodyParser()
 app.use(parserMiddleware)
 
+//
 app.get('/videos', (req: Request, res: Response) => {
     res.send(videos)
 })
+//
 app.post('/videos', (req: Request, res: Response) => {
+    let title = req.body.title
+    if(!title || typeof title !== 'string' || !title.trim()) {
+        res.status(400).send({
+            errorMessages: [{
+                "message": "Incorrect title",
+                "field": "title"
+            }]
+        })
+        return;
+    }
     const newVideo = {
         id: +(new Date()),
         title: req.body.title,
         author: req.body.author
     }
+    videos.push(newVideo)
     res.status(201).send(newVideo)
 })
 app.get('/videos/:videoId', (req: Request, res: Response) => {
     let video = videos.find(v => v.id === +req.params.videoId)
     video ? res.status(200).send(video) : res.send(404)
 })
+//
 app.put('/videos/:videoId', (req: Request, res: Response) => {
-    let video = videos.find(v => v.id === +req.params.id)
+    let title = req.body.title
+    if(!title || typeof title !== 'string' || !title.trim()) {
+        res.status(400).send({
+            errorMessages: [{
+                "message": "Incorrect title",
+                "field": "title"
+            }]
+        })
+        return;
+    }
+
+    const id = +req.params.videoId;
+    const video = videos.find(v => v.id === id)
     if (video) {
         video.title = req.body.title
-        res.send(video)
+        res.status(204).send(video)
     } else {
         res.send(404)
     }
